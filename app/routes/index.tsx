8fccb74e3523000f41cf7a2c7020a1a87ee3b410
mimichloee <mimichloee@gmail.com>
2022-02-22 00:47:52 +0900

    feat: add routes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,30 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+          <Link to="/about">About</Link>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+          <Link to="/blog">Blog</Link>
         </li>
       </ul>
     </div>
