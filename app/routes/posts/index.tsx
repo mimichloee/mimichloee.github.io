d5473fd5e8055318a6f211873965fe7597add144
mimichloee <mimichloee@gmail.com>
2022-02-27 21:53:36 +0900

    feat: add blog mdx

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,3 +1,25 @@
-export default function Posts() {
-  return <div>Blog main</div>;
+import { Link, useLoaderData } from 'remix';
+import { getPosts, Post } from '~/post';
+
+export const loader = async () => {
+  return getPosts();
+};
+
+function Posts() {
+  const posts = useLoaderData<Post[]>();
+
+  return (
+    <div className="posts">
+      <h1>chloe's Posts</h1>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default Posts;
