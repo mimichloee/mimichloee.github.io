51be8460bc4c49e8b8f69887ea469a85f4a1022b
mimichloee <mimichloee@gmail.com>
2022-02-27 22:17:01 +0900

    feat: add slug

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,22 @@
+import { useLoaderData } from '@remix-run/react';
+import invariant from 'tiny-invariant';
+import { getPost, Post } from '~/post';
+
+export const loader = async ({ params }: any) => {
+  invariant(params.slug, 'expected params.slug');
+  return getPost(params.slug);
+};
+
+export default function PostSlug() {
+  const post = useLoaderData<Post>();
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <section>
+        <ul>
+          <li>{post.date}</li>
+        </ul>
+      </section>
+    </div>
+  );
+}
