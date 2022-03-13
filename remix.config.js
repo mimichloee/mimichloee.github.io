16b69ff1a6a0c48fcc005838a5ef77a3142138b9
mimichloee <mimichloee@gmail.com>
2022-03-13 14:49:30 +0900

    feat: fix remix config

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: 'vercel',
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  ignoredRouteFiles: ['.*'],
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   serverBuildPath: 'api/index.js',
   publicPath: '/build/',
-  ignoredRouteFiles: ['.*'],
 };
