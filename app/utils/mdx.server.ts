decba0cad274365f0b0799c272b8ff06f7e564fb
mimichloee <mimichloee@gmail.com>
2022-03-20 20:39:14 +0900

    feat: replace highlight library

--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -1,18 +1,17 @@
 import { bundleMDX } from 'mdx-bundler';
 
 export async function bundleMDXPost(content: string) {
-  const { default: remarkPrism } = await import('remark-prism');
+  const { default: rehypeHighlight } = await import('rehype-highlight');
   const { default: remarkSlug } = await import('remark-slug');
 
   const { code, errors } = await bundleMDX({
     source: content,
     xdmOptions: (options) => {
-      options.remarkPlugins = [
-        ...(options?.remarkPlugins ?? []),
-        remarkPrism as any,
-        remarkSlug,
+      options.remarkPlugins = [...(options?.remarkPlugins ?? []), remarkSlug];
+      options.rehypePlugins = [
+        ...(options?.rehypePlugins ?? []),
+        rehypeHighlight,
       ];
-      options.rehypePlugins = [...(options?.rehypePlugins ?? [])];
       return options;
     },
   });
